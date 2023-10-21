fbe29bc6b77c119fdb9296fdd51c82dabd1ae513
Manachai06 <148631244+Manachai06@users.noreply.github.com>
2023-10-21 20:10:39 +0700

    Add files via upload

---
+++ NEW.js
@@ -0,0 +1,8 @@
+function Thank (){
+    if(document.querySelector('h1').innerHTML === 'Thank you for reading'){
+     document.querySelector('h1').innerHTML = '';
+     }
+     else {
+     document.querySelector('h1').innerHTML = 'Thank you for reading';
+     }
+    }
